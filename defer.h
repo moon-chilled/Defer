8f09259d136763a114b45dd00c1eb51e3334263c
Moonchild <elronnd@elronnd.net>
2020-02-15 15:57:25 -0700

    WIP version that uses computed goto.  Doesn't work because __COUNTER__ expands after token gluing, not before

--- defer.h
+++ defer.h
@@ -3,6 +3,42 @@
 
 #include <setjmp.h>
 
+#if defined(__GNUC__) && defined(DEFER_USE_COMPUTED_GOTO)
+
+#define Deferral \
+size_t _num_deferrals = 0; \
+void *_defer_return_loc = 0, *_deferrals[24] = {0}; /* TODO: make this number configurable? */ \
+
+#define Defer(block) _Defer(block, __COUNTER__)
+
+#define _Defer(block, n) do { \
+	_deferrals[_num_deferrals++] = && _defer_ini ## n; \
+	goto _defer_fini_ ## n; \
+	_defer_ini ## n: \
+		block; \
+		if (_num_deferrals) { \
+			goto *_deferrals[--_num_deferrals]; \
+		} else { \
+			goto *_defer_return_loc; \
+		} \
+	} \
+	_defer_fini_ ## n: \
+} while (0)
+
+#define Return(...) _Return(__COUNTER__, __VA_ARGS__)
+
+#define _Return(n, ...) do { \
+	if (_num_deferrals) { \
+		defer_return_loc = && _defer_fini_ ## n; \
+		goto *_deferrals[--_num_deferrals]; \
+	} \
+\
+	_defer_fini_ ## n: \
+	return __VA_ARGS__; \
+} while (0)
+
+#else
+
 #define Deferral \
 size_t _num_deferrals = 0; \
 jmp_buf _defer_return_loc = {0}, _deferrals[24] = {0}; /* TODO: make this number configurable? */
@@ -50,5 +86,7 @@ jmp_buf _defer_return_loc = {0}, _deferrals[24] = {0}; /* TODO: make this number
 	} \
 } while (0)
 #endif
+
+#endif /* __GNUC__ */
 
 #endif /*DEFER_H*/
